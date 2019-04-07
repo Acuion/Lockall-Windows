e0d5cfa2db4f0cf86a799072368ec4aed32bc2bd
Yuriy <natarur@yandex.com>
2019-04-08 01:53:23 +0300

    wip

--- Lockall-Windows/Forms/QrDisplayerWindow.cs
+++ Lockall-Windows/Forms/QrDisplayerWindow.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Lockall_Windows.Comm;
 using Newtonsoft.Json;
 using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Lockall_Windows.Forms
 {
