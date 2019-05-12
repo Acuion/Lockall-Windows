bce4758d89e16f74dbfa5f4c8768d8e3f4c0fb98
Yuriy <natarur@yandex.com>
2019-05-12 18:20:41 +0300

    pass gen system

--- Lockall-Windows/Forms/PasswordGetterForm.Designer.cs
+++ Lockall-Windows/Forms/PasswordGetterForm.Designer.cs
@@ -29,6 +29,19 @@
         private void InitializeComponent()
         {
             this.passwordBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.trackBarPasslen = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelPasslen = new System.Windows.Forms.Label();
+            this.checkBoxUppercase = new System.Windows.Forms.CheckBox();
+            this.checkBoxLowercase = new System.Windows.Forms.CheckBox();
+            this.checkBoxDigits = new System.Windows.Forms.CheckBox();
+            this.checkBoxSymbols = new System.Windows.Forms.CheckBox();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonVis = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPasslen)).BeginInit();
             this.SuspendLayout();
             // 
             // passwordBox
@@ -36,14 +49,139 @@
             this.passwordBox.Location = new System.Drawing.Point(12, 12);
             this.passwordBox.Name = "passwordBox";
             this.passwordBox.PasswordChar = '•';
-            this.passwordBox.Size = new System.Drawing.Size(314, 20);
+            this.passwordBox.Size = new System.Drawing.Size(243, 20);
             this.passwordBox.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonGenerate);
+            this.groupBox1.Controls.Add(this.checkBoxSymbols);
+            this.groupBox1.Controls.Add(this.checkBoxDigits);
+            this.groupBox1.Controls.Add(this.checkBoxLowercase);
+            this.groupBox1.Controls.Add(this.checkBoxUppercase);
+            this.groupBox1.Controls.Add(this.labelPasslen);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.trackBarPasslen);
+            this.groupBox1.Location = new System.Drawing.Point(12, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(315, 155);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Generate";
+            // 
+            // trackBarPasslen
+            // 
+            this.trackBarPasslen.Location = new System.Drawing.Point(6, 35);
+            this.trackBarPasslen.Maximum = 64;
+            this.trackBarPasslen.Minimum = 6;
+            this.trackBarPasslen.Name = "trackBarPasslen";
+            this.trackBarPasslen.Size = new System.Drawing.Size(303, 45);
+            this.trackBarPasslen.TabIndex = 2;
+            this.trackBarPasslen.Value = 12;
+            this.trackBarPasslen.Scroll += new System.EventHandler(this.trackBarPasslen_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Length:";
+            // 
+            // labelPasslen
+            // 
+            this.labelPasslen.AutoSize = true;
+            this.labelPasslen.Location = new System.Drawing.Point(55, 19);
+            this.labelPasslen.Name = "labelPasslen";
+            this.labelPasslen.Size = new System.Drawing.Size(19, 13);
+            this.labelPasslen.TabIndex = 4;
+            this.labelPasslen.Text = "12";
+            // 
+            // checkBoxUppercase
+            // 
+            this.checkBoxUppercase.AutoSize = true;
+            this.checkBoxUppercase.Checked = true;
+            this.checkBoxUppercase.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxUppercase.Location = new System.Drawing.Point(9, 74);
+            this.checkBoxUppercase.Name = "checkBoxUppercase";
+            this.checkBoxUppercase.Size = new System.Drawing.Size(125, 17);
+            this.checkBoxUppercase.TabIndex = 5;
+            this.checkBoxUppercase.Text = "Uppercase letters (A)";
+            this.checkBoxUppercase.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxLowercase
+            // 
+            this.checkBoxLowercase.AutoSize = true;
+            this.checkBoxLowercase.Checked = true;
+            this.checkBoxLowercase.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxLowercase.Location = new System.Drawing.Point(9, 97);
+            this.checkBoxLowercase.Name = "checkBoxLowercase";
+            this.checkBoxLowercase.Size = new System.Drawing.Size(124, 17);
+            this.checkBoxLowercase.TabIndex = 6;
+            this.checkBoxLowercase.Text = "Lowercase letters (a)";
+            this.checkBoxLowercase.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDigits
+            // 
+            this.checkBoxDigits.AutoSize = true;
+            this.checkBoxDigits.Checked = true;
+            this.checkBoxDigits.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxDigits.Location = new System.Drawing.Point(212, 74);
+            this.checkBoxDigits.Name = "checkBoxDigits";
+            this.checkBoxDigits.Size = new System.Drawing.Size(67, 17);
+            this.checkBoxDigits.TabIndex = 7;
+            this.checkBoxDigits.Text = "Digits (4)";
+            this.checkBoxDigits.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSymbols
+            // 
+            this.checkBoxSymbols.AutoSize = true;
+            this.checkBoxSymbols.Location = new System.Drawing.Point(212, 97);
+            this.checkBoxSymbols.Name = "checkBoxSymbols";
+            this.checkBoxSymbols.Size = new System.Drawing.Size(85, 17);
+            this.checkBoxSymbols.TabIndex = 8;
+            this.checkBoxSymbols.Text = "Symbols (@)";
+            this.checkBoxSymbols.UseVisualStyleBackColor = true;
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(9, 120);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(300, 23);
+            this.buttonGenerate.TabIndex = 9;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(297, 12);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(30, 20);
+            this.buttonOk.TabIndex = 2;
+            this.buttonOk.Text = "ok";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // buttonVis
+            // 
+            this.buttonVis.Location = new System.Drawing.Point(261, 12);
+            this.buttonVis.Name = "buttonVis";
+            this.buttonVis.Size = new System.Drawing.Size(30, 20);
+            this.buttonVis.TabIndex = 3;
+            this.buttonVis.Text = "Vis";
+            this.buttonVis.UseVisualStyleBackColor = true;
+            this.buttonVis.Click += new System.EventHandler(this.buttonVis_Click);
             // 
             // PasswordGetterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(339, 42);
+            this.ClientSize = new System.Drawing.Size(339, 204);
+            this.Controls.Add(this.buttonVis);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.passwordBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
@@ -52,6 +190,9 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PasswordGetterForm";
             this.TopMost = true;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPasslen)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -60,5 +201,16 @@
         #endregion
 
         private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelPasslen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBarPasslen;
+        private System.Windows.Forms.CheckBox checkBoxLowercase;
+        private System.Windows.Forms.CheckBox checkBoxUppercase;
+        private System.Windows.Forms.CheckBox checkBoxSymbols;
+        private System.Windows.Forms.CheckBox checkBoxDigits;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonVis;
     }
 }
--- Lockall-Windows/Forms/PasswordGetterForm.cs
+++ Lockall-Windows/Forms/PasswordGetterForm.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -31,14 +33,91 @@ namespace Lockall_Windows.Forms
             TopLevel = true;
         }
 
+        private void Finish()
+        {
+            if (string.Empty == passwordBox.Text.Trim())
+            {
+                return;
+            }
+            DialogResult = DialogResult.OK;
+            PasswordResult = passwordBox.Text;
+        }
+
         private void PasswordBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
             {
-                DialogResult = DialogResult.OK;
-                PasswordResult = passwordBox.Text;
+                Finish();
                 e.Handled = true;
             }
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            var upper = new List<string>
+            {
+                "A", "E", "I", "O", "U", "Y", "B", "C", "D", "F", "G", "H",
+                "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"
+            };
+            var lower = new List<string>
+            {
+                "a", "e", "i", "o", "u", "y", "b", "c", "d", "f", "g", "h",
+                "j", "k", "l", "m", "n", "p", "q", "r", "s","t", "v", "w", "x", "y", "z"
+            };
+            var digits = new List<string>
+            {
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+            };
+            var symbols = new List<string>
+            {
+                "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";",
+                "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"
+            };
+
+            var dict = new List<string>();
+            if (checkBoxUppercase.Checked)
+            {
+                dict.AddRange(upper);
+            }
+            if (checkBoxLowercase.Checked)
+            {
+                dict.AddRange(lower);
+            }
+            if (checkBoxDigits.Checked)
+            {
+                dict.AddRange(digits);
+            }
+            if (checkBoxSymbols.Checked)
+            {
+                dict.AddRange(symbols);
+            }
+
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                var buffer = new byte[4];
+                rng.GetBytes(buffer);
+                var rnd = new Random((buffer[0]) + (buffer[1] << 8) + (buffer[2] << 16) + (buffer[3] << 24));
+                passwordBox.Clear();
+                for (int i = 0; i < trackBarPasslen.Value; ++i)
+                {
+                    passwordBox.Text += dict[rnd.Next(dict.Count)];
+                }
+            }
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            Finish();
+        }
+
+        private void buttonVis_Click(object sender, EventArgs e)
+        {
+            passwordBox.PasswordChar = '\0' == passwordBox.PasswordChar ? '•' : '\0';
+        }
+
+        private void trackBarPasslen_Scroll(object sender, EventArgs e)
+        {
+            labelPasslen.Text = trackBarPasslen.Value.ToString();
         }
     }
 }
